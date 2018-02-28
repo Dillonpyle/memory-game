3a1c11cfdd461d5b968e699083172ff5aa45a353
Computer <computer@Computers-MacBook-Pro.local>
2018-02-27 20:27:12 -0800

    unit 10 added objects

--- js/main.js
+++ js/main.js
@@ -1,33 +1,55 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [flipCard];
+var cards = [
+  {
+    card: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    card: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    card: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    card: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
 
-var checkForMatch = function (){
+var cardsInPlay = [];
 
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
-	} else {
-	console.log("Sorry, try again.");
-	}
-}
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
+};
 
 var flipCard = function (cardId) {
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
+  console.log("User flipped " + cards[cardId].card);
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+  cardsInPlay.push(cards[cardId].card);
 
-console.log("User flipped " + cards[cardId]);
+  console.log(cards[cardId].cardImage)
 
-if (cardsInPlay.length === 2) {
-	alert("You found a match!");
-}
-else{
-	alert("Sorry try again.");
-}
-}
- 
- flipCard(0);
+  console.log(cards[cardId].suit)
+
+  if (cardsInPlay.length === 2) {
+   
+    checkForMatch();
+
+    cardsInPlay = [];
+  }
+};
+
+
+flipCard(0);
+
+flipCard(2);
